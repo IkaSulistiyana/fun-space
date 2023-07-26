86abee0eb0cdabcba3d28ab815fe051211d851e3
ikasulistiyana <ikasulistiyanamu@gmail.com>
2023-07-26 08:16:11 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,8 @@
+
+def luas_segitiga():
+    a = int(input("Masukaan alas segitiga: "))
+    t = int(input("Masukaan Tinggi segitiga:"))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+    luas_segitiga()
