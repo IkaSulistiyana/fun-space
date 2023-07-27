1c2f253d3021f9249f15c4684189e513cd7c6e70
Ikahsulistiyana <centralwallpaperdinding@gmail.com>
2023-07-27 11:33:03 +0700

    First Github Project

---
+++ ppp.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int(input("masukkan alas segitiga: "))
+    t = int(input("masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+luas_segitiga()
